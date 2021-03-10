df09e1959b626b16379e01876b5dcc322a49be74
ImmortalBrick <34840787+Kanexis@users.noreply.github.com>
2021-03-10 17:59:18 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CalWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel((k) =>
+                    {
+                        k.ListenAnyIP(5000);
+                        //k.ListenAnyIP(5001);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ wwwroot/js/CalenderScript.js
@@ -0,0 +1,33 @@
+﻿
+function selectDate(date) {
+    $('#calendar').updateCalendarOptions({
+        date: date
+    });
+    console.log("Single Click");
+}
+
+function onDoubleClick(date) {
+    $('#calendar').updateCalendarOptions({
+        date: date
+    });
+    $('#calendar-modal').iziModal('open');
+    console.log("Doublick done");
+}
+
+var defaultConfig = {
+    weekDayLength: 1,
+    date: new Date(),
+    onClickDate: selectDate,
+    onDoubleClickDate: onDoubleClick,
+    showYearDropdown: true,
+};
+
+$('#calendar').calendar(defaultConfig);
+$('.day').addClass('noselect');
+
+
+//Calender Modal
+$("#calendar-modal").iziModal({
+    padding: "2rem",
+    radius:5,
+});
